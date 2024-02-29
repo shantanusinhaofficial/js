6514f3ed8041a07ba7d5b62b835f6d781563765f
shantanusinha <107639795+shantanusinhaofficial@users.noreply.github.com>
2024-02-29 05:27:36 +0000

    conversion check

---
+++ conversion.js
@@ -0,0 +1,9 @@
+// let score = 33
+
+// console.log(typeof(score));
+// let valueinnumber = Number(score)
+// console.log(typeof valueinnumber);
+let someNumber = 35
+let stringNumber= string(someNumber)
+console.log(stringNumber)
+console.log(typeof stringNumber);
