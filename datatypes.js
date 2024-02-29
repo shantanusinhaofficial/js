19d3eeda80cd3dac70237882294fa42373e885a0
shantanusinha <107639795+shantanusinhaofficial@users.noreply.github.com>
2024-02-29 05:16:43 +0000

    datatypes

---
+++ datatypes.js
@@ -0,0 +1,15 @@
+console.log("shantanu")
+
+let name="shantanu"
+let age = 20
+let isLoggedIn = true
+let state = null
+//number => 2^53
+//bigint
+//string use in ""
+//boolean true and false 
+//NULL stand alone vALUE
+//undefined
+//symbol=> unique
+//object {} or []
+console.log(typeof "shantanu");
