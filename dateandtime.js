8d1ae638c6b056066f6ffb9f980edb23642b3ce4
shantanusinha <107639795+shantanusinhaofficial@users.noreply.github.com>
2024-04-18 10:26:51 +0000

    date

---
+++ dateandtime.js
@@ -0,0 +1,9 @@
+let mydate = new Date()
+console.log(mydate.toISOString());
+  
+
+let mytimestamp = Date.now()
+
+// console.log(mytimestamp);
+// console.log(mydate.getTime());
+console.log(Math.floor(mydate.now()/1000));
