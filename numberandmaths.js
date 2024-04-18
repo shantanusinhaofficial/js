fa1cc776db0f71bc8643c75c260f11cb94924f95
shantanusinha <107639795+shantanusinhaofficial@users.noreply.github.com>
2024-04-18 10:06:41 +0000

    math random

---
+++ numberandmaths.js
@@ -0,0 +1,11 @@
+const score = 400
+const balance = new Number(100)
+// console.log(balance);
+// console.log(balance.toString());
+// console.log(balance.toFixed(2));
+
+const hunderds = 100000
+// console.log(hunderds.toLocaleString());
+const min = 10
+const max = 20
+console.log(Math.floor(Math.random() * (max - min + 1)) + min)
