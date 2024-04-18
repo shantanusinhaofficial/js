473461651b02b0dae7ed5490ad1c31f9e39ad95b
shantanusinha <107639795+shantanusinhaofficial@users.noreply.github.com>
2024-04-18 05:06:18 +0000

    new str

---
+++ str.js
@@ -0,0 +1,7 @@
+const name = "shantanu"
+const repcount = 58;
+console.log(name + repcount);
+console.log(name.toUpperCase());
+console.log(name.charAt(5));
+const newstr=name.substring(0,5)
+console.log(newstr);
