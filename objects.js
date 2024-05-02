f5ac9d32128077ba14bbd80b94bc92cc94d7a062
shantanusinha <107639795+shantanusinhaofficial@users.noreply.github.com>
2024-05-02 04:12:05 +0000

    chnge

--- objects.js
+++ objects.js
@@ -13,3 +13,6 @@ jsUser.greeting = function(){
     console.log('hello js user,${this.name}');
 }
 console.log(jsUser.greeting());
+const newarr = [1,2,3,4,5,[1,5,4],[1,5,8]]
+// console.log(newarr.flat(Infinity));
+// console.log(newarr.slice(3));
